f13ab1e86d80e02c763d28ef3bf04c4a53c00adc
piyushprakash70 <prakashpiyush2003@gmail.com>
2026-06-23 15:12:39 +0530

    Initial Commit

---
+++ pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int *ptr = &a;           //stores the address
+
+    cout<<&a<<endl;         // give the address of a
+    cout<<ptr<<endl;        // give the same address of a
+    cout<<*(ptr)<<endl;     //give the value stored in the variable a
+    cout<<*(ptr+1)<<endl;
+    
+
+}
