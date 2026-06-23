42b1606e1aa119ef752c63ad4128ba6c04e98edc
piyushprakash70 <prakashpiyush2003@gmail.com>
2026-06-23 15:13:12 +0530

    Initial Commit

--- pointer.cpp
+++ pointer.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main()
 {
     int a = 10;
-    int *ptr = &a;           //stores the address
+    int *ptr = &a;          //stores the address
 
     cout<<&a<<endl;         // give the address of a
     cout<<ptr<<endl;        // give the same address of a
     cout<<*(ptr)<<endl;     //give the value stored in the variable a
-    cout<<*(ptr+1)<<endl;
-    
-
+    cout<<*(ptr+1)<<endl;   //garbage value
 }
