03acd4d2ff0071b2ada28234e09e43fd6b02e82c
piyushprakash70 <prakashpiyush2003@gmail.com>
2026-06-30 16:38:24 +0530

    Initial Commit

---
+++ 2d_array.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3][4];
+    //user input row wise
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    //user input column wise
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;i<3;j++)
+        {
+            cin>>arr[j][i];
+        }
+    }
+}
